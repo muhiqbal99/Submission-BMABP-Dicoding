dc7d2449d035fdcddb3bac58a3b17ebea9e345aa
Muhammad Iqbal <muhiqbal1290@gmail.com>
2021-04-17 09:17:39 +0700

    Ganti server

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require("./routes");
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.41.82',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.18.128',
         routes: {
           cors: {
             origin: ['*'],
