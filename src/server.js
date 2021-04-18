98aaea0db159a854b9e3e4e5056895d18a3b4722
Muhammad Iqbal <muhiqbal1290@gmail.com>
2021-04-18 13:52:27 +0700

    Fix some minor bug

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.18.128',
-        routes: {
-          cors: {
-            origin: ['*'],
-          },
-        },
-      });
-    server.route(routes);
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.18.128',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 init();
